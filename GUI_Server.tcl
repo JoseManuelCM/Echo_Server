9a814125d857d8c225cfc7b69e8ce55171693927
JoseManuelCM <mac970529@gmail.com>
2018-09-10 15:44:04 -0500

    controladores agregados

--- GUI_Server.tcl
+++ GUI_Server.tcl
@@ -1,8 +1,8 @@
-wm title . {Eco Client}
+wm title . {Eco Server}
 . configure -height 400
 . configure -width 320
-label .titulo -text "Cliente de Eco" -font {times -24 bold} -foreground blue
-button .conectar -text "Conectar" -font {times -15 bold} -foreground orange -background black -command hola
+label .titulo -text "Server de Eco" -font {times -24 bold} -foreground blue
+button .activar -text "Activar" -font {times -15 bold} -foreground orange -background black -command hola
 button .cerrar -text "Cerrar" -font {times -15 bold} -foreground orange -background black 
 label .nPort -foreground blue -text "-Número de puerto-" -justify left
 label .lestado -foreground blue -text "-Estado-" -justify left
@@ -12,7 +12,7 @@ message .estado -background white -text " " -padx 150 -pady 40 -font {Helvetica 
 message .mensaje_r -background white -text " " -padx 150 -pady 40 -font {Helvetica -12} -textvariable t_mensaje_r -justify right
 #-command "print 5"
 place .titulo -x 75 -y 5
-place .conectar -x 25 -y 40 
+place .activar -x 25 -y 40 
 place .cerrar -x 200 -y 40
 place .nPort -x 100 -y 75
 place .puerto -x 120 -y 90
